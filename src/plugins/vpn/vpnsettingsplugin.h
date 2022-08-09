c89a1e5c0b6cf76b7880e129000f2d2def0da132
Chupligin Sergey <neochapay@gmail.com>
2022-08-09 09:15:50 +0000

    [vpnplugin] Add proto

---
+++ src/plugins/vpn/vpnsettingsplugin.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022 Chupligin Sergey <neochapay@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef VPNSETTINGSPLUGIN_H
+#define VPNSETTINGSPLUGIN_H
+
+#include "glaciersettingsplugin.h"
+
+class VpnSettingsPlugin: public GlacierSettingsPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(GlacierSettingsPlugin)
+    Q_PLUGIN_METADATA(IID "Glacier.SettingsPlugin")
+public:
+    VpnSettingsPlugin(QObject *parent = nullptr);
+    PluginCategory category() { return PluginCategory::Network ;}
+    QString id() { return "vpn";}
+    QString title() { return tr("VPN");}
+    QString description() { return m_description;}
+    QString qmlPath() { return "/usr/share/glacier-settings/plugins/vpn/vpn.qml";}
+    QString icon() { return "/usr/share/glacier-settings/plugins/vpn/vpn.svg";}
+    bool enabled() { return m_enabled; };
+
+private:
+    bool m_enabled;
+    QString m_description;
+};
+
+#endif // VPNSETTINGSPLUGIN_H
