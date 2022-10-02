b36dc51352ee478d76a66fe833257bc5a0d4fb7c
Jozef Mlich <jmlich83@gmail.com>
2022-10-02 09:09:40 +0000

    add input translation files

---
+++ src/plugins/location/translations/location.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LocationSettingsPlugin</name>
+    <message>
+        <location filename="../locationsettingsplugin.h" line="34"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../locationsettingsplugin.h" line="35"/>
+        <source>GPS and other location services</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>location</name>
+    <message>
+        <location filename="../location.qml" line="40"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="64"/>
+        <source>Enable location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="73"/>
+        <source>Enable sattelites location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="83"/>
+        <source>Use sattelites location in flight mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="93"/>
+        <source>Enable online location services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="111"/>
+        <location filename="../location.qml" line="265"/>
+        <source>Latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="111"/>
+        <location filename="../location.qml" line="118"/>
+        <source>unavailable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="118"/>
+        <location filename="../location.qml" line="268"/>
+        <source>Longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="125"/>
+        <source>Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="277"/>
+        <source>Satellite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="279"/>
+        <source>Not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="281"/>
+        <source>Non-satellite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="283"/>
+        <source>Multiple</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../location.qml" line="284"/>
+        <source>source error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
