75057dd10f5b67e0851737833a6b692ee728d115
Jozef Mlich <jmlich83@gmail.com>
2022-10-24 17:25:44 +0000

    lupdate

--- src/plugins/powersave/translations/powersave.ts
+++ src/plugins/powersave/translations/powersave.ts
@@ -2,6 +2,24 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>BatteryHealth</name>
+    <message>
+        <location filename="../BatteryHealth.qml" line="36"/>
+        <source>Battery health</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../BatteryHealth.qml" line="49"/>
+        <source>Charger enable limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../BatteryHealth.qml" line="67"/>
+        <source>Charger disable limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PowerSaveSettingsPlugin</name>
     <message>
         <location filename="../powersavesettingsplugin.h" line="33"/>
@@ -22,13 +40,53 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../powersave.qml" line="73"/>
+        <source>Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="74"/>
+        <source>Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="75"/>
+        <source>Apply thresholds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../powersave.qml" line="76"/>
+        <source>Apply thresholds after full</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="81"/>
+        <source>Unknow battery status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="95"/>
+        <source>Charger mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="112"/>
         <source>Force power save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../powersave.qml" line="92"/>
+        <location filename="../powersave.qml" line="129"/>
         <source>Power save mode battery level threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="150"/>
+        <source>Battery health</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powersave.qml" line="151"/>
+        <source>Set charging limits to increase battery life</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
